e0c650fef655108cb070d18145c35d8e6ff0fc85
Bharani951 <bharani.22it@kct.ac.in>
2024-04-21 21:00:24 +0530

    done the task1

--- Task 3/script.js
+++ Task 3/script.js
@@ -1,17 +1,80 @@
-function appendValue(value) {
-  document.getElementById("display").value += value;
-}
-
-function clearDisplay() {
+document.getElementById("clear").onclick = function () {
   document.getElementById("display").value = "";
-}
+};
 
-function calculateResult() {
-  let expression = document.getElementById("display").value;
-  try {
-    document.getElementById("display").value = eval(expression);
-  } catch (error) {
-    alert("Invalid expression");
-    clearDisplay();
-  }
-}
+document.getElementById("delete").onclick = function () {
+  document.getElementById("display").value = document
+    .getElementById("display")
+    .value.toString()
+    .slice(0, -1);
+};
+
+document.getElementById("dot").onclick = function () {
+  document.getElementById("display").value += ".";
+};
+
+document.getElementById("divide").onclick = function () {
+  document.getElementById("display").value += "/";
+};
+
+document.getElementById("seven").onclick = function () {
+  document.getElementById("display").value += "7";
+};
+
+document.getElementById("eight").onclick = function () {
+  document.getElementById("display").value += "8";
+};
+
+document.getElementById("nine").onclick = function () {
+  document.getElementById("display").value += "9";
+};
+
+document.getElementById("multiply").onclick = function () {
+  document.getElementById("display").value += "*";
+};
+
+document.getElementById("four").onclick = function () {
+  document.getElementById("display").value += "4";
+};
+
+document.getElementById("five").onclick = function () {
+  document.getElementById("display").value += "5";
+};
+
+document.getElementById("six").onclick = function () {
+  document.getElementById("display").value += "6";
+};
+
+document.getElementById("subtract").onclick = function () {
+  document.getElementById("display").value += "-";
+};
+
+document.getElementById("one").onclick = function () {
+  document.getElementById("display").value += "1";
+};
+
+document.getElementById("two").onclick = function () {
+  document.getElementById("display").value += "2";
+};
+
+document.getElementById("three").onclick = function () {
+  document.getElementById("display").value += "3";
+};
+
+document.getElementById("add").onclick = function () {
+  document.getElementById("display").value += "+";
+};
+
+document.getElementById("doubleZero").onclick = function () {
+  document.getElementById("display").value += "00";
+};
+
+document.getElementById("zero").onclick = function () {
+  document.getElementById("display").value += "0";
+};
+
+document.getElementById("equal").onclick = function () {
+  document.getElementById("display").value = eval(
+    document.getElementById("display").value
+  );
+};
